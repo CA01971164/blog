d2d6be2898dc4f69d911b94280338699436391e2
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-08 17:48:37 +0900

    add all

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-
-function App() {
-  return (
-    <div>
-        <p>Hello World</p>
-    </div>
-  );
-}
+import React from "react";
+const App: React.FC = () => {
+  return <div>Hello,World</div>;
+};
 
 export default App;
+
+// import React from 'react';
+
+// const App: React.FC = () => {
+//   return (
+//     <div>a</div>
+//   );
+// }
+
+// export default App;
