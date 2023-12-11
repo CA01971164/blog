0670324e7e6a775712b9dfda8130988f14cca56a
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-11 13:54:57 +0900

    Componentization of the header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,36 @@
 import React from "react";
+
+const Head: React.FC = () => {
+  return (
+    <header>
+      <h1>Blog</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="">home</a>
+          </li>
+          <li>
+            <a href="">言語</a>
+          </li>
+          <li>
+            <a href="">カテゴリ</a>
+          </li>
+          <li>
+            <a href="">お問い合わせ</a>
+          </li>
+          <li>
+            <a href="">ログイン</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 const App: React.FC = () => {
   return (
     <>
-      <header>
-        <h1>Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="">home</a>
-            </li>
-            <li>
-              <a href="">言語</a>
-            </li>
-            <li>
-              <a href="">カテゴリ</a>
-            </li>
-            <li>
-              <a href="">お問い合わせ</a>
-            </li>
-            <li>
-              <a href="">ログイン</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Head />
       <main>
         <p>POST</p>
         <article className="blog-post">
@@ -40,26 +47,26 @@ const App: React.FC = () => {
           </header>
           <p>本文2</p>
         </article>
-       
-          <nav>
-            <ul>
-              <li>
-                <a href="page1.html">1</a>
-              </li>
-              <li>
-                <a href="page2.html">2</a>
-              </li>
-              <li>
-                <a href="page3.html">3</a>
-              </li>
-              <li>
-                <a href="page4.html">4</a>
-              </li>
-              <li>
-                <a href="page12.html">12</a>
-              </li>
-            </ul>
-          </nav>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="page1.html">1</a>
+            </li>
+            <li>
+              <a href="page2.html">2</a>
+            </li>
+            <li>
+              <a href="page3.html">3</a>
+            </li>
+            <li>
+              <a href="page4.html">4</a>
+            </li>
+            <li>
+              <a href="page12.html">12</a>
+            </li>
+          </ul>
+        </nav>
       </main>
       <footer>
         <p>Copyright © 2023</p>
