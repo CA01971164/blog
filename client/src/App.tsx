93b9508148d20c6169f6493e776fa620a109fdde
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-11 10:10:58 +0900

    Removed unused comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,4 @@ const App: React.FC = () => {
 
 export default App;
 
-// import React from 'react';
 
-// const App: React.FC = () => {
-//   return (
-//     <div>a</div>
-//   );
-// }
-
-// export default App;
