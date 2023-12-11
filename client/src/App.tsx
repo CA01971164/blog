94b854b6575baa42e118197f09d210c0616352bb
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-11 10:31:09 +0900

    added header in HTML

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 const App: React.FC = () => {
-  return <div>Hello,World</div>;
+  return (
+    <>
+      <header>
+        <button>home</button>
+        <form>
+          <label>検索:</label>
+          <input type="search"></input>
+          <input type="submit" value="検索" />
+        </form>
+        <form>
+          <select name="言語" id="">
+            <option value="日本語">日本語</option>
+            <option value="English">English</option>
+          </select>
+        </form>
+        <button>カテゴリ</button>
+        <button>ログイン</button>
+      </header>
+    </>
+  );
 };
 
 export default App;
-
-
