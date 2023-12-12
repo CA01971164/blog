a6008c46a49976856c29169c048c018adb0df540
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-12 14:09:57 +0900

    added usePostData

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,13 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { Article } from "./Components/Article";
-
-import { useState } from "react";
 import { Head } from "./Components/Head";
 import { Foot } from "./Components/Foot";
 
+import usePostData from "./Hooks/usePostData";
+
 // 全体を見せる表示
 const App: React.FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/posts");
-        console.log(response.data);
-        const arr = response.data;
-        setPosts(arr);
-      } catch (error) {
-        console.error("Error", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const { posts } = usePostData();
 
   return (
     <>
