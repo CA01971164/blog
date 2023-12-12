1947cf58b9277384d75cd4c7435f6a7c0cd1a1f8
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-12 15:52:32 +0900

    Updated Article

--- client/src/Components/Article.tsx
+++ client/src/Components/Article.tsx
@@ -1,3 +1,10 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 //投稿した記事を表示させる仕組み
 export const Article: React.FC<{ posts: Record<number, Post> }> = ({
   posts,
@@ -5,12 +12,18 @@ export const Article: React.FC<{ posts: Record<number, Post> }> = ({
   return (
     <>
       {Object.values(posts).map((post) => (
-        <article key={post.id} className="blog-post">
-          <header>
-            <h2>{post.title}</h2>
-          </header>
-          <p>{post.content}</p>
-        </article>
+        <Card sx={{ minWidth: 275, m: 2 }} key={post.id} className="blog-post">
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {post.title}
+            </Typography>
+            <Typography variant="body2">{post.content}</Typography>
+
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </CardContent>
+        </Card>
       ))}
     </>
   );
