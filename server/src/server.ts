dfeda5f10baf27cc48a72b8b80f72f60f0d71028
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-18 15:32:03 +0900

    added a routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import blogRouters from "./routes/blogRoutes";
 
 const app = express();
 
@@ -10,7 +11,8 @@ mongoose.connect("mongodb://localhost:27017/blog");
 
 app.use(bodyParser.json());
 
-// ルーティングを随時追加
+// ルーティングを追加
+app.use(`/users`, blogRouters);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
