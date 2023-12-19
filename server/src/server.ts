40113696b85f383c0f62c7c05a4b83ef95309f67
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-19 16:06:18 +0900

    deleted Login comp

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,6 @@ import blogRouters from "./routes/blogRoutes";
 const app = express();
 const path = require(`path`);
 
-const passport = require("passport");
-const LocalStrage = require("passport-local").Strategy;
-const session = require("express-session");
-const authRoutes = require("./routes/authRoutes");
-
 //mongoDBへの接続
 
 mongoose.connect("mongodb://127.0.0.1:27017/blog");
