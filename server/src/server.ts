004cb2b11493f8318649c3c146b526dec2743361
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2023-12-19 12:00:09 +0900

    added path

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,16 @@ import bodyParser from "body-parser";
 import blogRouters from "./routes/blogRoutes";
 
 const app = express();
+const path = require(`path`);
 
 //mongoDBへの接続
 
 mongoose.connect("mongodb://127.0.0.1:27017/blog");
 
 app.use(bodyParser.json());
+
+//静的ファイルの提供
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 // ルーティングを追加
 app.use(`/users`, blogRouters);
