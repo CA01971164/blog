f763327c6b9520df9b66c94dbfa26923e8b87f07
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2024-01-09 11:22:56 +0900

    add cors()

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/blog");
 
 app.use(bodyParser.json());
 
+// CORSをすべてのルートに対して有効化
+app.use(cors());
+
 //静的ファイルの提供
 app.use(express.static(path.join(__dirname, "../../client/build")));
 
@@ -22,3 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+function cors(): any {
+  throw new Error("Function not implemented.");
+}
+
