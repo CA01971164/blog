3a5c64e86e6bdb694e0ac3c3a14232d848254cf6
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2024-02-07 10:21:05 +0900

    client file deletion

--- client/src/Components/Foot.tsx
+++
@@ -1,7 +0,0 @@
-export const Foot: React.FC = () => {
-    return (
-      <footer>
-        <p>Copyright © 2023</p>
-      </footer>
-    );
-  };
--- client/src/type/Post.d.ts
+++
@@ -1,7 +0,0 @@
-// josoデータで受け取る型の指定
-type Post = {
-  [x: string]: ReactNode;
-  _id: string;
-  title: string;
-  summary: string;
-};
